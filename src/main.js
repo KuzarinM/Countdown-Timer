b2047b4a86b229f2f40c573f594861d21f96f8ec
M Kuzarin <kuzarinm@mail.ru>
2023-08-20 12:27:50 +0400

    test

--- src/main.js
+++ src/main.js
@@ -9,17 +9,17 @@ const app = createApp(App)
 
 //Для тех людей, кто заглянет сюда. Да, это костыль, и ещё какой. Размером, с наблюдаемую вселенную. Да, это именно что так. Но,
 //по факту я не виноват. Vue 3 вообше с .env работает плохо(требует префикс и т. п.) Поэтому, ну вот так как-то.... А что поделать
-window.process = {
-env: {
-    POSTGRES_URL:"postgres://default:7cUDFEjN5VtH@ep-calm-math-49465601-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-    POSTGRES_PRISMA_URL:"postgres://default:7cUDFEjN5VtH@ep-calm-math-49465601-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
-    POSTGRES_URL_NON_POOLING:"postgres://default:7cUDFEjN5VtH@ep-calm-math-49465601.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-    POSTGRES_USER:"default",
-    POSTGRES_HOST:"ep-calm-math-49465601-pooler.us-east-1.postgres.vercel-storage.com",
-    POSTGRES_PASSWORD:"7cUDFEjN5VtH",
-    POSTGRES_DATABASE:"verceldb",
-}
-}
+// window.process = {
+// env: {
+//     POSTGRES_URL:"postgres://default:7cUDFEjN5VtH@ep-calm-math-49465601-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+//     POSTGRES_PRISMA_URL:"postgres://default:7cUDFEjN5VtH@ep-calm-math-49465601-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
+//     POSTGRES_URL_NON_POOLING:"postgres://default:7cUDFEjN5VtH@ep-calm-math-49465601.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+//     POSTGRES_USER:"default",
+//     POSTGRES_HOST:"ep-calm-math-49465601-pooler.us-east-1.postgres.vercel-storage.com",
+//     POSTGRES_PASSWORD:"7cUDFEjN5VtH",
+//     POSTGRES_DATABASE:"verceldb",
+// }
+// }
 
 app.use(router)
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenvExpand from 'dotenv-expand';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+
+
+export default ({ mode }) => {
+  // This check is important!
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+    dotenvExpand.expand({ parsed: env });
+  }
+
+  return defineConfig({ 
+    plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  }}
+  )
+}
+
